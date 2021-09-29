5d5a8ace277d36a42eaceafe1531c48561c5f588
zhrbzd <zhrbbzd@gmail.com>
2021-09-29 14:27:00 +0330

    bg_changer

---
+++ index.js
@@ -0,0 +1,9 @@
+function change() {
+    let h2 = document.getElementById('h2');
+    h2.style.display = 'none';
+    let h4 = document.getElementById('h4');
+    h4.style.display = 'none';
+    let colorCode = '#' + Math.floor(Math.random() * 1000000);
+    document.body.style.backgroundColor = colorCode;
+    document.getElementById("colorname").innerHTML = colorCode;
+}
