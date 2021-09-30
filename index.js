3614ee23b9bf5a977486f334118e39db405a5e3a
zhrbzd <zhrbbzd@gmail.com>
2021-09-30 11:54:06 +0330

    syntax

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function change() {
     h2.style.display = 'none';
     let h4 = document.getElementById('h4');
     h4.style.display = 'none';
-    let colorCode = '#' + Math.floor(Math.random() * 1000000);
+    let colorCode = '#' + Math.floor(Math.random() * 10**6);
     document.body.style.backgroundColor = colorCode;
     document.getElementById("colorname").innerHTML = colorCode;
 }
